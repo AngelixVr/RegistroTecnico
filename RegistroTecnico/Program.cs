9453a86f2f90ec20d2f26804fcd3c828b879a18b
Angelix Vasquez Robles <angelixvasquez1234@gmail.com>
2025-01-17 11:22:07 -0400

    Componente Razor realizado

--- RegistroTecnico/Program.cs
+++ RegistroTecnico/Program.cs
@@ -1,28 +1,46 @@
+using Microsoft.EntityFrameworkCore;
 using RegistroTecnico.Components;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace RegistroTecnico
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+
+            var ConStr = builder.Configuration.GetConnectionString("sqlConStr");
+            builder.Services.AddDbContextFactory<Contexto>(o=> o.UseSqlServer(ConStr));
+
+            //Inyeccion del Service
+            builder.Services.AddScoped<TecnicosService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
